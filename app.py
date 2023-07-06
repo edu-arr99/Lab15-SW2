1a6a2ceab8007bc8ddbe2f74640e8f5bf7081a9e
edu-arr99 <75773399+edu-arr99@users.noreply.github.com>
2023-07-05 19:54:37 -0500

    linter

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-
 @app.route("/hello")
 def hello():
     return jsonify("hello"), 200
 
+
 @app.route("/suma/<a>/<b>")
 def suma(a, b):
-    return jsonify(int(a)+int(b)), 200
+    return jsonify(int(a) + int(b)), 200
 
 
 if __name__ == "__main__":
--- test_app.py
+++ test_app.py
@@ -1,12 +1,14 @@
 import pytest
 from app import app
 
+
 def test_hello():
     response = app.test_client().get("/hello")
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == '"hello"\n'
+    assert (response.data.decode('utf-8') == '"hello"\n')
+
 
 def test_suma():
     response = app.test_client().get("/suma/4/5")
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == '9\n'
+    assert (response.data.decode('utf-8') == '9\n')
